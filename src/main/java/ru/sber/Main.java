3286ca911eecf5bc7ca681ba8e8f4056a8009365
Kiva Aleksey <adolfp@mail.ru>
2021-10-18 16:39:13 +0700

    Task3 find max population in array

--- src/main/java/ru/sber/Main.java
+++ src/main/java/ru/sber/Main.java
@@ -1,25 +1,13 @@
 package ru.sber;
 
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<City> cities = CityUtils.parse();
 
-        System.out.println("Сортировка списка городов по наименованию в алфавитном порядке по" +
-                " убыванию без учета регистра: ");
-        cities.stream()
-                .sorted(Comparator.comparing(City::getNameLowerCase).reversed())
-                .forEach(System.out::println);
-
-        System.out.println("\nСортировка списка городов по федеральному округу и наименованию" +
-                " города внутри каждого федерального округа в алфавитном порядке по убыванию" +
-                " с учетом регистра: ");
-        cities.stream()
-                .sorted(Comparator.comparing(City::getDistrict)
-                        .thenComparing(City::getName).reversed())
-                .forEach(System.out::println);
+        int[] maxPopulation = CityUtils.maxPopulation(cities);
+        System.out.println("[" + maxPopulation[0]+ "] = " + maxPopulation[1]);
     }
 }
 
